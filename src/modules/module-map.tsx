40198cd4b5192f901b15508d8d8cec334b45992d
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2023-10-22 21:43:49 +0300

    Changed import dependency for render in

--- src/modules/module-map.tsx
+++ src/modules/module-map.tsx
@@ -2,10 +2,14 @@ import { Map } from '@/components/map';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const init = (root: Element) => {
+const init = (rootId: string = ".rootMap") => {
+  const root = document.querySelector(rootId);
+
   if (!root) {
-    console.log("Please provide the root element for render");
-  } else {
+    console.log("Document element for reneder module not found");
+  }
+
+  if (root) {
     ReactDOM.render(<Map />, root);
   }
 };
