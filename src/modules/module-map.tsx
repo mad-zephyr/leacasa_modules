7d04f947578e432316ac271dfcb6c1e8f21f331e
vasilii yatsyuk <72764693+mad-zephyr@users.noreply.github.com>
2023-10-22 15:38:07 +0300

    Add querySelector to mapModule

--- src/modules/module-map.tsx
+++ src/modules/module-map.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const init = (rootId: string) => {
-  const root = document.getElementById(rootId);
-  if (root) {
-    ReactDOM.render(<Map />, root);
-  }
+  console.log("rootId", rootId)
+
+  const root = document.querySelector(rootId);
+
+  console.log("Root", root)
+
+  root && ReactDOM.render(<Map />, root);
 };
 
 export { init };
